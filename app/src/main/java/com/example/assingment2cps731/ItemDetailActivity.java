f7d1bd9c375e6b13aa2aa5ba714f7f9e9df2462a
BrandenSang <brandensang@live.com>
2020-10-11 20:54:19 -0400

    set up the start up image for 5 seconds

--- app/src/main/java/com/example/assingment2cps731/ItemDetailActivity.java
+++ app/src/main/java/com/example/assingment2cps731/ItemDetailActivity.java
@@ -15,12 +15,14 @@ import androidx.appcompat.app.ActionBar;
 
 import android.view.MenuItem;
 
+
 /**
  * An activity representing a single Item detail screen. This
  * activity is only used on narrow width devices. On tablet-size devices,
  * item details are presented side-by-side with a list of items
  * in a {@link ItemListActivity}.
  */
+
 public class ItemDetailActivity extends AppCompatActivity {
 
     @Override
