1504622fe58d3c3bad9c709dd96eec9301883a5b
BrandenSang <brandensang@live.com>
2020-10-11 22:14:39 -0400

    Added the music and corresponding code in the splash activity

--- app/src/main/java/com/example/assingment2cps731/SplashActivity.java
+++ app/src/main/java/com/example/assingment2cps731/SplashActivity.java
@@ -1,12 +1,16 @@
 package com.example.assingment2cps731;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 
-public class SplashActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 5000;
+    MediaPlayer mySong;
 
     /**
      * Called when the activity is first created.
@@ -17,16 +21,20 @@ public class SplashActivity extends Activity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.splash);
 
+            mySong = new MediaPlayer();
+            mySong = MediaPlayer.create(this, R.raw.splashintro);
 
-        /* New Handler to start the Menu-Activity
-         * and close this Splash-Screen after some seconds.*/
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
+                mySong.start();
+
                 Intent mainIntent = new Intent(SplashActivity.this,ItemListActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
+
+                mySong.stop();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
